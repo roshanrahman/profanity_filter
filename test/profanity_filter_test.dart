e8429dcd479e58c265dd33869bceedc5ccc9b9ca
Roshan <roshanrahman6399@gmail.com>
2021-06-29 22:01:23 +0530

    Added test cases

--- test/profanity_filter_test.dart
+++ test/profanity_filter_test.dart
@@ -74,6 +74,25 @@ void main() {
     final filter = ProfanityFilter();
     expect(filter.hasProfanity('practitioner assessment'), false);
     expect(filter.censor('practitioner assessment'), 'practitioner assessment');
-     expect(filter.getAllProfanity('practitioner assessment'), []);
+    expect(filter.getAllProfanity('practitioner assessment'), []);
+  });
+
+  test('large paragraph test', () {
+    final paragraph = """
+    It is a long established fact that a reader will be distracted by the
+    readable content of a page when looking at its layout. The point of using 
+    Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+    as opposed to using 'Content here, content here', making it look like
+    readable English. Many desktop publishing packages and web page editors 
+    now use Lorem Ipsum as their default model text, and a search for 'lorem 
+    ipsum' will uncover many web sites still in their infancy. Various versions
+    have evolved over the years, sometimes by accident, sometimes on purpose
+    (injected humour and the like).
+    """ *
+        200;
+    final filter = ProfanityFilter();
+    expect(filter.hasProfanity(paragraph), false);
+    expect(filter.censor(paragraph), paragraph);
+    expect(filter.getAllProfanity(paragraph), []);
   });
 }
